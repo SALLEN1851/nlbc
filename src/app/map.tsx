377236f739c69dc2026bfe7ad86f5d4b9b0d5f5b
Scott Allen <sallen1851@gmail.com>
2024-08-23 14:53:57 -0400

    update

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -125,7 +125,7 @@ const Map: React.FC = () => {
         addPolygon(map, polygon398, 'polygon-398', '#05B4DF');
         addPolygon(map, polygon442, 'polygon-442', '#05B4DF');
         addPolygon(map, polygon411Coordinates, 'polygon-411', '#05B4DF');
-        addPolygon(map, RDOF2, 'rdof2', '#05B4DF');
+        // addPolygon(map, RDOF2, 'rdof2', '#05B4DF');
       });
 
       const geocoder = new MapboxGeocoder({
