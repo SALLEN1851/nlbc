6ed42da0613dd679f93d2f7db3efad635fb8c99a
SALLEN1851 <sallen1851@gmail.com>
2024-08-31 22:16:13 -0400

    speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Footer from './footer';
 import Map from './map'; 
 import SearchAddressForm from './SearchAddressForm'; 
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Main() {
   return (
@@ -12,6 +13,7 @@ export default function Main() {
       <SearchAddressForm />
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
